9811de51cf242fce4ed8f8d378971c5a80f807ed
香考拉 <t4cgbb@gmail.com>
2024-06-04 08:02:42 -0500

    Update settings.py
    test

--- ChengHuei_Web/settings.py
+++ ChengHuei_Web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-50lgyb@$43fj$j5p^si#dcnpfw$5u@ukp-zun2j=7neij18q9r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
